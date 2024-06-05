c6d7338a57c484d743a08873427b3a6db7b5135b
Rama Sathya Sai <rchandik@gitam.in>
2024-06-05 23:43:06 +0530

    chore: Refactor database copying logic to exclude certain collections

--- db_copier.py
+++ db_copier.py
@@ -12,13 +12,10 @@ dev_db = dev_client["test"]
 prod_collections = list(prod_db.list_collection_names())
 
 for collection_name in prod_collections:
-    if collection_name == "events" or collection_name == "eventconfigs":
-        continue
-    else:
-        prod_collection = prod_db.get_collection(collection_name)
-        dev_collection = dev_db.get_collection(collection_name)
-        dev_collection.drop()
-        print(f"Copying {collection_name} from prod to dev")
-        documents = list(prod_collection.find())
-        for document in documents:
-            dev_collection.insert_one(document)
+    prod_collection = prod_db.get_collection(collection_name)
+    dev_collection = dev_db.get_collection(collection_name)
+    dev_collection.drop()
+    print(f"Copying {collection_name} from prod to dev")
+    documents = list(prod_collection.find())
+    for document in documents:
+        dev_collection.insert_one(document)

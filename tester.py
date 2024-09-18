15e8fc860e2f1e713ed7d581a0fa62dbea18c9a4
Rama Sathya Sai <rchandik@gitam.in>
2024-09-18 20:29:31 +0530

    Refactor blank_inserter.py to insert blank timings for all experts and days

--- tester.py
+++ tester.py
@@ -1,46 +1,34 @@
-from config import prodcalls_collection
+from config import devcalls_collection as collection
 from time import sleep as s
 from bson import ObjectId
 import datetime
 
-experts = [
-    ObjectId("66046a3d42f04a057fa21034"),
-    ObjectId("6604694542f04a057fa2100f")
-]
+document = {
+    "callId": "2f8eb444-09df-4b5e-ae48-ed2628f01e2d",
+    "duration": "0:05:51",
+    "expert": ObjectId("6604694542f04a057fa2100f"),
+    "failedReason": "",
+    "initiatedTime": datetime.datetime(2024, 9, 18, 11, 32, 29, 127000),
+    "recording_url": "https://sr.knowlarity.com/vr/fetchsound/?callid%3D2f8eb444-09df-4b5e-ae48-ed2628f01e2d",
+    "status": "successfull",
+    "transferDuration": "00:05:39",
+    "user": ObjectId("66c828b7dff284cbc80223e7"),
+}
 
-i = 0
+time = datetime.datetime.now()
+collection.insert_one(document)
+print("Inserted @", time)
 
-while True:
-    i += 1
-    if i == 10:
-        break
-    document = {
-        "callId": "f072938a-8235-48cc-ba09-dca020ccad40",
-        "duration": "0:1:32",
-        "expert": experts[0],
-        "failedReason": "",
-        "initiatedTime": datetime.datetime(2024, 6, 13, 14, 20, 20, 988000),
-        "recording_url": "https://sr.knowlarity.com/vr/fetchsound/?callid%3Df072938a-8235-48cc-ba09-dca020ccad40",
-        "status": "successfull",
-        "transferDuration": "00:1:23",
-        "user": ObjectId("660b893a9f28ee9c2c007d06"),
-    }
+s(20)
 
-    time = datetime.datetime.now()
-    prodcalls_collection.insert_one(document)
-    print("Inserted @", time)
+collection.update_one(
+    {"callId": "f072938a-8235-48cc-ba09-dca020ccad40",
+     "expert": ObjectId("6604694542f04a057fa2100f")},
+    {"$set": {"status": "failed", "failedReason": "User not available"}})
+time = datetime.datetime.now()
+print("Updated @", time)
 
-    s(20)
+s(5)
 
-    prodcalls_collection.update_one(
-        {"callId": "f072938a-8235-48cc-ba09-dca020ccad40",
-            "expert": experts[0]},
-        {"$set": {"status": "failed", "failedReason": "User not available"}})
-    time = datetime.datetime.now()
-    print("Updated @", time)
-
-    s(5)
-
-    prodcalls_collection.delete_one(
-        {"callId": "f072938a-8235-48cc-ba09-dca020ccad40", "expert": experts[0]})
-    print("Deleted")
+collection.delete_one({"callId": "f072938a-8235-48cc-ba09-dca020ccad40"})
+print("Deleted")

96564faa3acb395a76262f0239c15fdb02a41006
Rama Sathya Sai <rchandik@gitam.in>
2024-10-10 01:02:06 +0530

    Refactor users_profile_corrector.py to update user profiles based on specific criteria

--- users_profile_corrector.py
+++ users_profile_corrector.py
@@ -6,7 +6,7 @@ true_users = 0
 false_users = 0
 
 for user in users:
-    if "name" in user and "city" in user and "birthDate" in user and user["name"] not in ["", None] and user["city"] not in ["", None] and user["birthDate"] not in ["", None]:
+    if "name" in user and "birthDate" in user and user["name"] not in ["", None] and user["birthDate"] not in ["", None]:
         collection.update_one({"_id": user["_id"]}, {
                               "$set": {"profileCompleted": True}})
         true_users += 1

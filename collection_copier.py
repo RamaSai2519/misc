f24999e42600385f24c90bbd180b503c081b7f66
RamaSai2519 <rchandik@gitam.in>
2025-01-13 11:04:34 +0530

    Rename collections in collection_copier.py to migrate documents from 'system_prompts' to 'prompt_proposals'

--- collection_copier.py
+++ collection_copier.py
@@ -2,14 +2,14 @@ from config import prod_client
 
 db = prod_client['embeddings']
 
-old_name = 'histories'
-new_name = 'old_histories'
+old_name = 'system_prompts'
+new_name = 'prompt_proposals'
 
 old_collection = db[old_name]
 new_collection = db[new_name]
 
 new_collection.drop()
-query = {"createdAt": {"$regex": "^.{11,}$"}}
+query = {}
 documents = list(old_collection.find(query))
 if documents and len(documents) > 0:
     update = new_collection.insert_many(documents)

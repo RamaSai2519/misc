c08209b475978f21049425ed3ff6fd91a2e32198
RamaSai2519 <rchandik@gitam.in>
2025-03-31 11:27:22 +0530

    Update subproject reference and refactor CSV export script to utilize database collections

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
-import pandas as pd
+from shared.db.users import get_user_collection, get_meta_collection
+from shared.db.chat import get_histories_collection
+import numpy as np
 
 
-def load_and_sort_csv(file_path):
-    df = pd.read_csv(file_path)
-    df['createdDate'] = pd.to_datetime(df['createdDate'])
-    sorted_df = df.sort_values(by='createdDate', ascending=False)
-
-    return sorted_df
+meta_collection = get_meta_collection()
+users_collection = get_user_collection()
+histories_collection = get_histories_collection()
 
 
-file_path = 'output1.csv'
-sorted_df = load_and_sort_csv(file_path)
+query = {'userStatus': {
+    '$in': ['not_interested_user', 'not_interested_calls_user']}}
+user_ids = meta_collection.distinct('user', query)
 
-sorted_df.to_csv(file_path, index=False)
+user_phones = users_collection.distinct(
+    'phoneNumber', {'_id': {'$in': user_ids}})
+
+docs = []
+for phone in user_phones:
+    query = {'phoneNumber': phone}
+    doc = histories_collection.find_one(query)
+    if doc:
+        docs.append(phone)
+
+output_file = 'output.csv'
+np.savetxt(output_file, docs, delimiter=",", fmt='%s',
+           header="PhoneNumber", comments='')
+print(f"Exported {len(docs)} phone numbers to {output_file}")
